577db4b5ec9605dc8a58de667a4fce62716f588c
Maxim <maximbotpy@gmail.com>
2024-03-29 21:35:26 +0300

    добавил отображение html страницы

--- Back/src/app/encrypt/router/api_v1/router.py
+++ Back/src/app/encrypt/router/api_v1/router.py
@@ -1,8 +1,10 @@
+from fastapi import Request
 from fastapi import APIRouter
+from core.config import template
 
 router = APIRouter(tags=["encrypt"])
 
 
 @router.get("/")
-async def get_home_page():
-    return "home page"
+async def get_home_page(request: Request):
+    return template.TemplateResponse("/main.html", {"request": request})

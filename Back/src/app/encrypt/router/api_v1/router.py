69afed0c70b9b1a89e338160045bd77cf9739af8
Maxim <maximbotpy@gmail.com>
2024-03-30 00:20:27 +0300

    добавил post запрос

--- Back/src/app/encrypt/router/api_v1/router.py
+++ Back/src/app/encrypt/router/api_v1/router.py
@@ -1,6 +1,7 @@
-from fastapi import Request
-from fastapi import APIRouter
+from fastapi import Request, UploadFile
 from core.config import template
+from fastapi import APIRouter, Depends
+from src.app.encrypt.schemas.schemas import CheckPassword
 
 router = APIRouter(tags=["encrypt"])
 
@@ -8,3 +9,8 @@ router = APIRouter(tags=["encrypt"])
 @router.get("/")
 async def get_home_page(request: Request):
     return template.TemplateResponse("/main.html", {"request": request})
+
+
+@router.post("/encrypt-file")
+async def encrypt_file(file: UploadFile, password=Depends(CheckPassword)):
+    return file

0b4724c0fa1573d7c7c4431d26f17064c7d2fff2
Maxim <maximbotpy@gmail.com>
2024-03-30 15:15:32 +0300

    добавил валидацию для пароля и файла

--- Back/src/app/encrypt/schemas/schemas.py
+++ Back/src/app/encrypt/schemas/schemas.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
 from typing_extensions import Annotated
 from fastapi.param_functions import Form
+from pydantic import BaseModel, ConfigDict
+from pydantic.functional_validators import model_validator
 
 
-class FileSchemas(BaseModel):
+class CheckPassword(BaseModel):
     password: Annotated[str, Form(min_length=8, max_length=20)]
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CheckFeli(BaseModel):
+    file_name: str
+    file_size: int
+    model_config = ConfigDict(from_attributes=True)

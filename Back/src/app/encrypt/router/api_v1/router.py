5d6969fa7eb249dda6cb56ee0ac33a91c5b82f83
Maxim <maximbotpy@gmail.com>
2024-04-01 17:36:46 +0300

    добавил отлавливание исключения при неверном пароле

--- Back/src/app/encrypt/router/api_v1/router.py
+++ Back/src/app/encrypt/router/api_v1/router.py
@@ -1,10 +1,11 @@
+from fastapi import HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi import APIRouter, Depends, Request, UploadFile, File
 
 from core.config import template
 from core.operation import save_file
-from src.app.encrypt.operation.encode_file import encrypt
-from src.app.encrypt.schemas.schemas import PasswordSchemas
+from src.app.encrypt.operations import encrypt
+from src.app.encrypt.schemas.schemas import PasswordSchemas, FileSchemas
 
 router = APIRouter(tags=["encrypt"])
 
@@ -14,8 +15,13 @@ async def get_home_page(request: Request):
     return template.TemplateResponse("/main.html", {"request": request})
 
 
-@router.post("/encrypt-file")
-async def encrypt_file(file: UploadFile = File(...), password: PasswordSchemas = Depends()):
+@router.post("/file", status_code=status.HTTP_200_OK)
+async def file_operations(password: PasswordSchemas = Depends(), file: UploadFile = File(...)):
+    FileSchemas(name=file.filename, size=file.size)
     await save_file(file_name=file.filename, file_path="save_file", file=file.file.read())
-    path = encrypt(file_path=f"save_file/{file.filename}", password=password.password)
-    return FileResponse(path=path, filename=f"{file.filename}.aes", media_type='multipart/form-data')
+    try:
+        path = encrypt(file_path=f"save_file/{file.filename}", password=password.password)
+        return FileResponse(path=path, filename=path, media_type='multipart/form-data')
+
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Неверный пароль")

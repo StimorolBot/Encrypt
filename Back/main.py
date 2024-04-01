304fa78b9cc8fa9fc0deb6ff011945e200a06cf4
Maxim <maximbotpy@gmail.com>
2024-04-01 23:08:06 +0300

    подключил роутер и добавил связь с фронтом

--- Back/main.py
+++ Back/main.py
@@ -3,12 +3,15 @@ import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from fastapi_cache import FastAPICache
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 
 from core.config import redis
 from src.app.encrypt.router.api_v1.router import router
+from src.app.auth.router.api_v1.router import auth_router
 
 
 @asynccontextmanager
@@ -19,7 +22,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Encrypt")
 app.include_router(router)
+app.include_router(auth_router)
 app.mount("/", StaticFiles(directory="../Front/"), name="css", )
+
+origins = ["http://localhost:8080", ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Authorization"],
+)
 
 
 async def main():

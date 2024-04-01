0165bea8de12589051d66f9559a567956e43e20b
Maxim <maximbotpy@gmail.com>
2024-04-01 22:21:38 +0300

    добавил класс Config для получения данных из .env

--- Back/core/config.py
+++ Back/core/config.py
@@ -1,3 +1,25 @@
+from redis import asyncio as aioredis
 from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+class Config(BaseSettings):
+    DB_USER: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    model_config = SettingsConfigDict(env_file="core/.env")
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+config = Config()
 template = Jinja2Templates(directory="../Front/html/")
+redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)

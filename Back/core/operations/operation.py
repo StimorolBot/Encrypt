15abf24a6da6127f676da74ab92ce421c42ca01c
Maxim <maximbotpy@gmail.com>
2024-04-01 23:08:57 +0300

    добавил генерацию uuid

--- Back/core/operations/operation.py
+++ Back/core/operations/operation.py
@@ -1,6 +1,11 @@
 import aiofiles
+from uuid import uuid4
 
 
 async def save_file(file_name: str, file_path: str, file):
     async with aiofiles.open(f"{file_path}/{file_name}", "wb") as f:
         await f.write(file)
+
+
+def generate_uuid():
+    return str(uuid4())

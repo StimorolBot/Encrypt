32422f69158f516da9b27c7178a3ca0629ed2a84
Maxim <maximbotpy@gmail.com>
2024-04-03 17:06:49 +0300

    добавил хэширование пароля и чтение из бд одной записи

--- Back/core/operations/crud.py
+++ Back/core/operations/crud.py
@@ -1,4 +1,6 @@
 from typing import TYPE_CHECKING
+from sqlalchemy import select, func
+from core.config import pwd_context
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,7 +9,19 @@ if TYPE_CHECKING:
 
 class Crud:
     @staticmethod
-    async def create(session: "AsyncSession", data_dict: dict, table: "DeclarativeAttributeIntercept"):
+    async def create(session: "AsyncSession", data_dict: dict,
+                     table: "DeclarativeAttributeIntercept") -> "DeclarativeAttributeIntercept":
+        if data_dict["password"]:
+            data_dict["password"] = pwd_context.hash(data_dict["password"])
+
         stmt = table(**data_dict)
         session.add(stmt)
         await session.commit()
+        return stmt
+
+    @staticmethod
+    async def read_one(session: "AsyncSession", table: "DeclarativeAttributeIntercept",
+                       field: "DeclarativeAttributeIntercept", value: str):
+        query = select(table).where(func.lower(field) == func.lower(value))
+        results = await session.execute(query)
+        return results.unique().scalar_one_or_none()

b4f64ffcb98cb03b91647577609af7ec8079511b
Maxim <maximbotpy@gmail.com>
2024-04-03 17:07:36 +0300

    добавил поля

--- Back/src/app/auth/schemas/aurt.py
+++ Back/src/app/auth/schemas/aurt.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class RegisterSchemas(BaseModel):
     user_name: str
     password: str
     email: EmailStr
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    model_config = ConfigDict(from_attributes=True)

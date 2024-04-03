f17bfe01a99c6dd8e9ae9480dbd30731dc84fb7f
Maxim <maximbotpy@gmail.com>
2024-04-03 21:44:29 +0300

    добавил функции получения текущего пользователя и переназначения параметров пользователя

--- Back/src/app/auth/user_manager.py
+++ Back/src/app/auth/user_manager.py
@@ -1,8 +1,18 @@
 from jose import jwt
+from typing import TYPE_CHECKING
 from datetime import datetime, timedelta, timezone
 
-from core.config import config, pwd_context
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
+
+from src.app.auth.models.user import UserTable
+
+from core.operations.crud import Crud
+from core.db import get_async_session
+from core.config import config, pwd_context, cookie_scheme
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserManager:
@@ -26,3 +36,18 @@ class UserManager:
             return False
         else:
             await self.verify_password(password, user.password)
+
+    @staticmethod
+    async def config_user(user_dict: dict) -> dict:
+        user_dict["password"] = pwd_context.hash(user_dict["password"])
+        user_dict["is_active"] = False
+        user_dict["is_superuser"] = False
+        user_dict["is_verified"] = False
+
+        return user_dict
+
+    @staticmethod
+    async def get_current_user(session: "AsyncSession" = Depends(get_async_session), cookie_session: str = Depends(cookie_scheme)) -> dict:
+        decode_jwt = jwt.decode(key=config.SECRET_KEY, token=cookie_session)
+        current_user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=decode_jwt["email"])
+        return current_user.__dict__

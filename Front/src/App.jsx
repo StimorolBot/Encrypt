92aa77d60e494c140270db8765fcf171a90d150d
Maxim <maximbotpy@gmail.com>
2024-04-04 21:58:36 +0300

    подключил компаненты формы и понели

---
+++ Front/src/App.jsx
@@ -0,0 +1,11 @@
+import {Form} from "./components/Form.jsx";
+import {Sidebar} from "./components/Sidebar.jsx";
+
+export function App() {
+    return (
+        <>
+            <Form/>
+            <Sidebar/>
+        </>
+    )
+}

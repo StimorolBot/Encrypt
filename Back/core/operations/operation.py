7903015bf5853ad5482ca0c1981d520168698c43
Maxim <maximbotpy@gmail.com>
2024-04-05 20:25:21 +0300

    добавил функцию создания директории

--- Back/core/operations/operation.py
+++ Back/core/operations/operation.py
@@ -1,8 +1,15 @@
+import os
 import aiofiles
 from uuid import uuid4
 
 
+async def check_dir(file_path: str):
+    if os.path.isdir(file_path) is False:
+        os.mkdir(file_path)
+
+
 async def save_file(file_name: str, file_path: str, file):
+    await check_dir(file_path)
     async with aiofiles.open(f"{file_path}/{file_name}", "wb") as f:
         await f.write(file)
 

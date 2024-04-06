9e8edb253867c4ef4c3a172de3d94faaf06072cb
Maxim <maximbotpy@gmail.com>
2024-04-06 13:55:14 +0300

    изменил ссылки для обращения

--- Back/main.py
+++ Back/main.py
@@ -25,7 +25,7 @@ app.include_router(router)
 app.include_router(auth_router)
 app.mount("/", StaticFiles(directory="../Front/"), name="css", )
 
-origins = ["http://localhost:8080", ]
+origins = ["http://127.0.0.1:5173", "http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,

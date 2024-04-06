f6ef9180c3f834c97df3ea888194645b3396015e
Maxim <maximbotpy@gmail.com>
2024-04-07 00:04:21 +0300

    добавил возврат пользователя

--- Back/src/app/auth/router/api_v1/router.py
+++ Back/src/app/auth/router/api_v1/router.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 
 from src.app.auth.models.user import UserTable
 from src.app.auth.user_manager import UserManager
-from src.app.auth.schemas.auth import UserCreate
+from src.app.auth.schemas.auth import UserCreate, UserRead
 
 from core.config import config
 from core.operations.crud import Crud
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 auth_router = APIRouter(tags=["auth"])
 
 
-@auth_router.post("/register", response_model=UserCreate, status_code=status.HTTP_201_CREATED)
+@auth_router.post("/register", response_model=UserRead, status_code=status.HTTP_201_CREATED)
 async def register_user(user_schemas: Annotated[UserCreate, Depends()], session: "AsyncSession" = Depends(get_async_session)):
     user = await Crud.read_one(table=UserTable, session=session, field=UserTable.email, value=user_schemas.email)
 
@@ -25,24 +25,26 @@ async def register_user(user_schemas: Annotated[UserCreate, Depends()], session:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Пользователь с такой почтой уже существует")
     user_dict = await UserManager.config_user(user_dict=user_schemas.model_dump())
     create_user = await Crud.create(data_dict=user_dict, session=session, table=UserTable)
-
     return create_user
 
 
-@auth_router.post("/login", status_code=status.HTTP_200_OK)
+@auth_router.post("/login", status_code=status.HTTP_200_OK, response_model=UserRead)
 async def login_user(response: Response, user_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                      session: "AsyncSession" = Depends(get_async_session)):  # в OAuth2PasswordRequestForm нет поля email
     user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=user_data.username)
-
+    # сделать пользователя активным
     if await UserManager().check_user(user=user, password=user_data.password) is False:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Неверный логин/пароль")
 
     access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = await UserManager.create_access_token(data={"email": user.email}, expires_delta=access_token_expires)
-    return response.set_cookie(key="access_token", value=access_token)
+    response.set_cookie(key="access_token", value=access_token)
+
+    return user
 
 
 @auth_router.post("/logout", status_code=status.HTTP_200_OK)
-async def logout_user(response: Response):
+async def logout_user(response: Response, current_user: UserRead = Depends(UserManager.get_current_user)) -> str:
     response.delete_cookie(key="access_token")
-    return "OK"
+    # сделать пользователя неактивным
+    return f"Пользователь {current_user.user_name} вышел из системы"

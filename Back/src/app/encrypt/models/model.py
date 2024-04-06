0324a878c090b8434331a208367f7ee6921511de
Maxim <maximbotpy@gmail.com>
2024-04-06 14:20:54 +0300

    разделил таблицу путей и файлов на 2 разные

--- Back/src/app/encrypt/models/model.py
+++ Back/src/app/encrypt/models/model.py
@@ -1,24 +1,32 @@
 import datetime
 from uuid import UUID
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 from sqlalchemy import func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.model.declarative_base import Base
-from core.operations.operation import generate_uuid
 
 if TYPE_CHECKING:
     from src.app.auth.models.user import UserTable
 
 
-class FileTable(Base):
-    __tablename__ = "File_Table"
+class PathTable(Base):
+    __tablename__ = "Path_Table"
 
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=generate_uuid)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_id: Mapped[UUID] = mapped_column(ForeignKey("User_Table.id"))
-    path: Mapped[str] = mapped_column(index=True)
-    file_name: Mapped[str] = mapped_column(index=True, unique=True)
+    path: Mapped[str] = mapped_column(index=True, unique=True)
     time: Mapped[datetime.datetime] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
 
     user: Mapped["UserTable"] = relationship("UserTable", back_populates="files")
+    file_name: Mapped[List["FileTable"]] = relationship("FileTable", back_populates="path")
+
+
+class FileTable(Base):
+    __tablename__ = "File_Table"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    path_id: Mapped[int] = mapped_column(ForeignKey("File_Table.id"))
+    file_name: Mapped[str] = mapped_column(index=True, unique=True)
+
+    path: Mapped["PathTable"] = relationship("PathTable", back_populates="file_name")

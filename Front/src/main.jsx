e1df63f2b1af95214d0340e788eae3d7e40744ec
Maxim <maximbotpy@gmail.com>
2024-04-08 23:39:45 +0300

    добавил роутинг

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import {App} from './App.jsx'
 
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>, 
+    },
+    {
+        path: "/test",
+        element: <h2>TEST</h2>, 
+    },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <RouterProvider router={router}/>
+
 )

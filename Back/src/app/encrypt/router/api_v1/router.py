a80054ff051972084ed924726270cab24a4a327b
Maxim <maximbotpy@gmail.com>
2024-04-08 23:43:43 +0300

    переписал функцию file_operations

--- Back/src/app/encrypt/router/api_v1/router.py
+++ Back/src/app/encrypt/router/api_v1/router.py
@@ -1,47 +1,33 @@
 from typing import TYPE_CHECKING
 from fastapi.responses import FileResponse
-from fastapi import APIRouter, Request, UploadFile, File, Form, HTTPException, status, Depends
+from fastapi import APIRouter, UploadFile, File, Form, status, Depends, Request
 
-from core.operations.crud import Crud
 from core.db import get_async_session
-from core.config import template, config
-from core.operations.operation import save_file
 
 from src.app.encrypt.operations import encrypt
 from src.app.auth.user_manager import UserManager
-from src.app.encrypt.models.model import FileTable
+from src.app.encrypt.file_manager import FileManager
 from src.app.encrypt.schemas.schemas import EncryptSchemas
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from src.app.auth.schemas.auth import UserRead
 
 router = APIRouter(tags=["encrypt"])
 
 
-@router.get("/") #current_user: dict = Depends(UserManager.get_current_user),
-async def get_home_page(request: Request,
-                        session: "AsyncSession" = Depends(get_async_session)):
-    await Crud.read_all(table=FileTable, session=session)
-    return template.TemplateResponse("/index.html", {"request": request})
+@router.get("/")
+async def get_home_page(re: Request, token: dict = Depends(UserManager.get_jwt_token), session: "AsyncSession" = Depends(get_async_session), ):
+    print(re.cookies["access_token"]) #
+    return await FileManager.get_info(session=session, email=token["email"])
 
 
 @router.post("/file", status_code=status.HTTP_200_OK)
 async def file_operations(password: str = Form(...), upload_file: UploadFile = File(...),
-                          current_user: dict = Depends(UserManager.get_current_user),
-                          session: "AsyncSession" = Depends(get_async_session)):
-    file_name = upload_file.filename
-    EncryptSchemas(file_name=file_name, file_size=upload_file.size, password=password)
-    file_path = f"{config.BASE_PATH}/{current_user['email']}"
-    current_file = await Crud.read_one(session=session, table=FileTable, field=FileTable.file_name, value=file_name)
-
-    if current_file is not None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Файл с таким именем уже существует")
-    await save_file(file_name=file_name, file_path=file_path, file=upload_file.file.read())
-
-    try:
-        path = encrypt(file_path=f"{file_path}/{file_name}", password=password)
-        data = {"user_id": current_user["id"], "path": file_path, "file_name": file_name}
-        await Crud.create(session=session, table=FileTable, data_dict=data)
-        return FileResponse(path=path, filename=path, media_type='multipart/form-data')
-    except ValueError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Неверный пароль")
+                          current_user: "UserRead" = Depends(UserManager.get_current_user),
+                          session: "AsyncSession" = Depends(get_async_session)) -> FileResponse:
+    EncryptSchemas(file_name=upload_file.filename, file_size=upload_file.size, password=password)
+    await FileManager().check_exists_file(session=session, file_name=upload_file.filename, dir_=current_user.email)
+    await FileManager().save_file(dir_=current_user.email, file_name=upload_file.filename, file=upload_file, session=session)
+    output_path, output_file = await encrypt(dir_=current_user.email, file_name=upload_file.filename, password=password)
+    return FileResponse(path=output_path, filename=output_file, media_type='multipart/form-data')

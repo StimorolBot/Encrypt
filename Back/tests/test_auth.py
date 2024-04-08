3781bcc1eca2e4e9e2b8c7de223bb0290b561c6f
Maxim <maximbotpy@gmail.com>
2024-04-08 23:31:24 +0300

    добавил положительные тесты для аутентификации

---
+++ Back/tests/test_auth.py
@@ -0,0 +1,23 @@
+from tests.conftest import ac
+from typing import TYPE_CHECKING
+from fastapi import status
+
+if TYPE_CHECKING:
+    from httpx import AsyncClient
+
+
+# pytest  -vv -s  tests/test_auth.py::TestAuth - запуск тестов
+class TestAuth:
+    async def test_register(self, ac: "AsyncClient"):
+        user_data = {"email": "test_11_22@test.com", "user_name": "nametest", "password": "passwordtest"}
+        response = await ac.post("/register", json=user_data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    async def test_login(self, ac: "AsyncClient"):
+        user_data = {"username": "test_11_22@test.com", "password": "passwordtest"}
+        response = await ac.post("/login", data=user_data)
+        assert response.status_code == status.HTTP_200_OK
+
+    async def test_logout(self, ac: "AsyncClient"):
+        response = await ac.post("/logout")
+        assert response.status_code == status.HTTP_200_OK

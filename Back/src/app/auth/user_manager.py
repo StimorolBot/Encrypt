538b85384e811bc1db0d47b9e2c795dce0f7270b
Maxim <maximbotpy@gmail.com>
2024-04-08 23:33:18 +0300

    разделил функцию get_current_user на две отдельные

--- Back/src/app/auth/user_manager.py
+++ Back/src/app/auth/user_manager.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta, timezone
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
+from src.app.auth.schemas.auth import UserRead
 from src.app.auth.models.user import UserTable
 
 from core.operations.crud import Crud
@@ -13,7 +14,6 @@ from core.config import config, pwd_context, cookie_scheme
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
-    from src.app.auth.schemas.auth import UserRead
 
 
 class UserManager:
@@ -48,10 +48,14 @@ class UserManager:
         return user_dict
 
     @staticmethod
-    async def get_current_user(session: "AsyncSession" = Depends(get_async_session), cookie_session: str = Depends(cookie_scheme)) -> dict:
-        decode_jwt = jwt.decode(key=config.SECRET_KEY, token=cookie_session)
-        current_user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=decode_jwt["email"])
-        return current_user.__dict__
+    async def get_jwt_token(cookie_session: str = Depends(cookie_scheme)):
+        return jwt.decode(key=config.SECRET_KEY, token=cookie_session)
+
+    @staticmethod
+    async def get_current_user(session: "AsyncSession" = Depends(get_async_session), jwt_token=Depends(get_jwt_token)) -> UserRead:  # вызвать get_jwt_token
+        token = await jwt_token
+        current_user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=token["email"])
+        return UserRead(**current_user.__dict__)
 
     @staticmethod
     async def on_after_register(user: "UserRead"):

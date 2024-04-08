8d615b4435db8091b9984ea98a408f12fa20fe7f
Maxim <maximbotpy@gmail.com>
2024-04-08 23:45:17 +0300

    переименовал атрибуты

--- Back/src/app/encrypt/models/model.py
+++ Back/src/app/encrypt/models/model.py
@@ -1,5 +1,4 @@
 import datetime
-from uuid import UUID
 from typing import TYPE_CHECKING, List
 
 from sqlalchemy import func, ForeignKey
@@ -15,18 +14,19 @@ class PathTable(Base):
     __tablename__ = "Path_Table"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[UUID] = mapped_column(ForeignKey("User_Table.id"))
+    email: Mapped[str] = mapped_column(ForeignKey("User_Table.email"), index=True, unique=True)
     path: Mapped[str] = mapped_column(index=True, unique=True)
     time: Mapped[datetime.datetime] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
 
-    user: Mapped["UserTable"] = relationship("UserTable", back_populates="files")
-    file_name: Mapped[List["FileTable"]] = relationship("FileTable", back_populates="path")
+    user: Mapped["UserTable"] = relationship("UserTable", back_populates="path")
+    file_name: Mapped[List["FileTable"]] = relationship(back_populates="path")
 
 
 class FileTable(Base):
     __tablename__ = "File_Table"
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    path_id: Mapped[int] = mapped_column(ForeignKey("File_Table.id"))
-    file_name: Mapped[str] = mapped_column(index=True, unique=True)
 
-    path: Mapped["PathTable"] = relationship("PathTable", back_populates="file_name")
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[int] = mapped_column(ForeignKey("Path_Table.email"))
+    name: Mapped[str] = mapped_column(index=True, unique=True)
+
+    path: Mapped["PathTable"] = relationship(back_populates="file_name")

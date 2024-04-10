1e5252cdcc1fa7fb05e05aa669b45add8150437b
Maxim <maximbotpy@gmail.com>
2024-04-10 20:20:05 +0300

    перенес роутер в отдельный файл

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,20 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import {App} from './App.jsx'
-
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>, 
-    },
-    {
-        path: "/test",
-        element: <h2>TEST</h2>, 
-    },
-]);
+import {RouterProvider } from "react-router-dom";
+import router from './config/router';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(

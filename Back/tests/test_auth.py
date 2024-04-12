682c09bbc4bafe87698e6b65d74983d50c32c4b5
Maxim <maximbotpy@gmail.com>
2024-04-12 23:28:43 +0300

    добавил негативные тесты

--- Back/tests/test_auth.py
+++ Back/tests/test_auth.py
@@ -7,17 +7,30 @@ if TYPE_CHECKING:
 
 
 # pytest  -vv -s  tests/test_auth.py::TestAuth - запуск тестов
-class TestAuth:
+class TestAuthPos:
     async def test_register(self, ac: "AsyncClient"):
         user_data = {"email": "test_11_22@test.com", "user_name": "nametest", "password": "passwordtest"}
         response = await ac.post("/register", json=user_data)
         assert response.status_code == status.HTTP_201_CREATED
 
     async def test_login(self, ac: "AsyncClient"):
-        user_data = {"username": "test_11_22@test.com", "password": "passwordtest"}
-        response = await ac.post("/login", data=user_data)
+        user_data = {"email": "test_11_22@test.com", "password": "passwordtest"}
+        response = await ac.post("/login", json=user_data)
         assert response.status_code == status.HTTP_200_OK
 
     async def test_logout(self, ac: "AsyncClient"):
         response = await ac.post("/logout")
         assert response.status_code == status.HTTP_200_OK
+
+
+class TestAuthNeg:
+    async def test_register(self, ac: "AsyncClient"):
+        user_data = {"email": "test_11_22@test.com", "user_name": "nametest", "password": "passwordtest"}
+        response = await ac.post("/register", json=user_data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    async def test_login(self, ac: "AsyncClient"):
+        user_data = {"email": "test_11_22@test.com", "password": "passwordtest-123"}
+        response = await ac.post("/login", json=user_data)
+        assert response.status_code == status.HTTP_200_OK
+

1be480159b5bd637b079b31694d7a67c6b51a420
Maxim <maximbotpy@gmail.com>
2024-04-17 23:12:17 +0300

    внес незначительные изменения

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {RouterProvider } from "react-router-dom";
-import router from './config/router';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import router from '/src/router/router';
+import { RouterProvider } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(

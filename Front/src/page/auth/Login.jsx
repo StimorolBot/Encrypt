350aa655cb3cb995be82e4f0635b928b63de18b4
Maxim <maximbotpy@gmail.com>
2024-04-17 23:25:02 +0300

    добавил страницу для входа

---
+++ Front/src/page/auth/Login.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import api from "/src/api/config";
+import { redirect } from "react-router-dom";
+import { MainBtn } from "/src/components/ui/btn/MainBtn";
+import { MainForm } from "/src/components/ui/form/MainForm";
+import { MainInput } from "/src/components/ui/input/MainInput";
+
+import "/src/style/components/page/auth/login.sass";
+
+
+export function Login() {
+    const [userData, setUserData] = useState({"email": "", "password":""});
+ 
+    const postlogin = async (event) => {
+        event.preventDefault();
+        
+        await api.post("/login", userData)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) =>{
+                console.log(error);
+            });
+        };
+
+    return (
+        <div className="wrapper wrapper__body">
+            <MainForm onSubmit={postlogin}>
+                <MainInput lblText={"Логин"} maxLength={24}
+                    type="text" placeholder=" " required
+                    onChange={(event) => setUserData({...userData, email: event.target.value})}
+                />
+                <MainInput lblText={"Пароль"} maxLength={24}
+                    type="password" placeholder=" " required
+                    onChange={(event) => setUserData({...userData, password: event.target.value})}
+                /> 
+                <MainBtn>Войти</MainBtn>
+            </MainForm>
+        </div>
+    );
+}

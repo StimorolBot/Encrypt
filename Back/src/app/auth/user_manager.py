700a77d375a1c422486e319eeab2100f996eeeef
Maxim <maximbotpy@gmail.com>
2024-04-20 21:49:49 +0300

    изменил алгоритм шифрования на RS256, добавил получение пользователя по access_token и выдачу access_token по refresh_token

--- Back/src/app/auth/user_manager.py
+++ Back/src/app/auth/user_manager.py
@@ -1,42 +1,34 @@
-from jose import jwt
-from typing import TYPE_CHECKING
-from datetime import datetime, timedelta, timezone
+from typing import TYPE_CHECKING, Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status, Header, Cookie
 from fastapi.security import OAuth2PasswordRequestForm
 
-from src.app.auth.schemas.auth import UserRead
+from src.app.auth.token import JWTToken
+from src.app.auth.token_type import TokenType
 from src.app.auth.models.user import UserTable
+from src.app.auth.schemas.auth import UserRead, UserLogin
 
 from core.operations.crud import Crud
 from core.db import get_async_session
-from core.config import config, pwd_context, cookie_scheme
+from core.setting import pwd_context
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class UserManager:
-    @staticmethod
-    async def create_access_token(data: dict, expires_delta: timedelta | None = None):
-        to_encode = data.copy()
-        if expires_delta:
-            expire = datetime.now(timezone.utc) + expires_delta
-        else:
-            expire = datetime.now(timezone.utc) + timedelta(minutes=60)
-        to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
-        return encoded_jwt, expire
-
     @classmethod
-    async def verify_password(cls, plain_password, hashed_password):
-        return pwd_context.verify(plain_password, hashed_password)
-
-    async def check_user(self, user: OAuth2PasswordRequestForm, password: str) -> bool:
+    async def verify_password(cls, user: OAuth2PasswordRequestForm, password: str) -> bool:
         if user is None:
-            return False
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверный логин/пароль")
         else:
-            return await self.verify_password(password, user.password)
+            return pwd_context.verify(password, user.password)
+
+    async def check_user(self, data_login: UserLogin, session: "AsyncSession" = Depends(get_async_session)):
+        user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=data_login.email)
+        if await self.verify_password(user=user, password=data_login.password) is False:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверный логин/пароль")
+        return user
 
     @staticmethod
     async def config_user(user_dict: dict) -> dict:
@@ -49,12 +41,19 @@ class UserManager:
         return user_dict
 
     @staticmethod
-    async def get_jwt_token(cookie_session: str = Depends(cookie_scheme)):
-        return jwt.decode(key=config.SECRET_KEY, token=cookie_session)
+    async def get_current_user_for_refresh_token(refresh_token: str | None = Cookie(default=None),
+                                                 session: "AsyncSession" = Depends(get_async_session)):
+
+        payload = await JWTToken.validate_token_type(token=refresh_token, token_type=TokenType.REFRESH.value)
+        current_user: UserRead = await Crud.read_one(table=UserTable, session=session, field=UserTable.id, value=payload["sub"])
+        access_token = await JWTToken.create_token(token_type=TokenType.ACCESS.value,
+                                                   token_data={"sub": str(current_user.id), "email": current_user.email})
+        return access_token
 
     @staticmethod
-    async def get_current_user(session: "AsyncSession" = Depends(get_async_session), jwt_token=Depends(get_jwt_token)) -> UserRead:  # вызвать get_jwt_token
-        jwt_token = await jwt_token
-        current_user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=jwt_token["email"])
-        return UserRead(**current_user.__dict__)
+    async def get_current_user(access_token: Annotated[str | None, Header(alias="Authorization")] = None,
+                               session: "AsyncSession" = Depends(get_async_session)) -> UserRead:
 
+        payload = await JWTToken.validate_token_type(token=access_token, token_type=TokenType.ACCESS.value)
+        current_user: UserRead = await Crud.read_one(table=UserTable, session=session, field=UserTable.email, value=payload["email"])
+        return current_user

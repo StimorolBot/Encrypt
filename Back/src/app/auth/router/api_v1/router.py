66a42daef66a13e01ffa2bfc961144a42b744483
Maxim <maximbotpy@gmail.com>
2024-04-20 22:05:03 +0300

    в функции входа добавил создание access и refresh токенов, добавил функцию обновления access по refresh

--- Back/src/app/auth/router/api_v1/router.py
+++ Back/src/app/auth/router/api_v1/router.py
@@ -1,21 +1,24 @@
 from datetime import timedelta
-from typing import TYPE_CHECKING, Annotated
-from fastapi.security import OAuth2PasswordRequestForm
+from typing import TYPE_CHECKING
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 
+from src.app.auth.token import JWTToken
+from src.app.auth.token_type import TokenType
 from src.app.auth.models.user import UserTable
 from src.app.auth.user_manager import UserManager
+from src.app.auth.schemas.token import TokenSchemas
 from src.app.auth.schemas.auth import UserCreate, UserRead
 
-from core.config import config
+from core.setting import setting
 from core.logger import user_logger
 from core.operations.crud import Crud
 from core.db import get_async_session
+from core.operations.operation import get_seconds
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-auth_router = APIRouter(tags=["auth"])
+auth_router = APIRouter(tags=["auth"], prefix="/auth")
 
 
 @auth_router.post("/register", response_model=UserRead, status_code=status.HTTP_201_CREATED)
@@ -24,32 +27,55 @@ async def register_user(user_schemas: UserCreate, session: "AsyncSession" = Depe
 
     if user is not None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Пользователь с такой почтой уже существует")
+
     user_dict = await UserManager.config_user(user_dict=user_schemas.model_dump())
     create_user = await Crud.create(data_dict=user_dict, session=session, table=UserTable)
     user_logger.info(f"Пользователь {user_schemas.email} создал аккаунт")
+
     return create_user
 
 
-@auth_router.post("/login", status_code=status.HTTP_200_OK, response_model=UserRead)
-async def login_user(response: Response, user_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-                     session: "AsyncSession" = Depends(get_async_session)):  # в OAuth2PasswordRequestForm нет поля email
-    user = await Crud.read_one(session=session, table=UserTable, field=UserTable.email, value=user_data.username)
+@auth_router.post("/login", status_code=status.HTTP_200_OK, response_model=TokenSchemas, response_model_exclude_none=True)
+async def login_user(response: Response, user: UserRead = Depends(UserManager().check_user)):
+    access_token = await JWTToken.create_token(token_type=TokenType.ACCESS.value,
+                                               token_data={"sub": str(user.id), "email": user.email})
 
-    if await UserManager().check_user(user=user, password=user_data.password) is False:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Неверный логин/пароль")
+    refresh_token = await JWTToken.create_token(token_type=TokenType.REFRESH.value, token_data={"sub": str(user.id)},
+                                                expire_timedelta=timedelta(days=setting.auth_jwt.refresh_token_expire_days))
 
-    access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = await UserManager.create_access_token(data={"email": user.email}, expires_delta=access_token_expires)
-    response.set_cookie(key="access_token", value=access_token)
-    await Crud.update(session=session, table=UserTable, field=UserTable.email, field_val=user_data.username, data={"is_active": True})
-    user_logger.info(f"Пользователь {user_data.username} вошел в систему")
-    return user
+    exp = get_seconds(setting.auth_jwt.refresh_token_expire_days)
+    response.set_cookie(key="refresh_token", value=refresh_token, max_age=exp, expires=exp, path="/", httponly=True, samesite="none")
+    user_logger.info(f"Пользователь {user.email} вошел в систему")
+
+    return TokenSchemas(access_token=access_token, refresh_token=refresh_token)
 
 
 @auth_router.post("/logout", status_code=status.HTTP_200_OK)
-async def logout_user(response: Response, current_user: UserRead = Depends(UserManager.get_current_user),
-                      session: "AsyncSession" = Depends(get_async_session)):
-    response.delete_cookie(key="access_token")
+async def logout_user(response: Response, session: "AsyncSession" = Depends(get_async_session),
+                      current_user: "UserRead" = Depends(UserManager.get_current_user)):
+    response.delete_cookie(key="refresh_token")
     await Crud.update(session=session, table=UserTable, field=UserTable.email, field_val=current_user.email, data={"is_active": False})
-    user_logger.info(f"Пользователь {current_user.email} вышел из системы")
-    return status.HTTP_200_OK
+
+
+@auth_router.post("/refresh/", response_model=TokenSchemas, response_model_exclude_none=True)
+async def refresh_jwt_token(token=Depends(UserManager.get_current_user_for_refresh_token)):
+    return TokenSchemas(access_token=token)
+
+
+""" 
+tokens = await UserManager.create_token(data={"email": user.email}, token_config=config.CONFIG_TOKEN, algorithm=config.ALGORITHM)
+
+    # не обновлять токен если с ним все норм
+    await Crud.update(
+        session=session, table=UserTable, field=UserTable.email,
+        field_val=user_login.email, data={"is_active": True, "refresh_token": tokens["refresh"]}
+    )
+
+    # expires доработать !
+    response.set_cookie(key="refresh_token", value=tokens["refresh"], httponly=True, expires=121212, domain="http://localhost:5173", path="/login")
+    user_logger.info(f"Пользователь {user_login.email} вошел в систему")
+
+    print(response.set_cookie.__dict__)
+
+    return [user_login, {"access_token": tokens["access"]}]"""
+

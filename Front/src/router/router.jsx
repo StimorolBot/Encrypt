10b9e7998c038dd020f6d52d2b6e0a53186a8770
Maxim <maximbotpy@gmail.com>
2024-04-20 21:53:31 +0300

    добавил дочерние элементы к пути /auth/

--- Front/src/router/router.jsx
+++ Front/src/router/router.jsx
@@ -1,28 +1,35 @@
 import { App } from '/src/App.jsx'
-import { createBrowserRouter } from "react-router-dom";
 import { Login } from '/src/page/auth/Login';
 import { Register } from '/src/page/auth/Register';
+import { createBrowserRouter } from "react-router-dom";
+import { Header } from "/src/components/header/Header";
 import { PageNotFound } from "/src/page/error/PageNotFound";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App/>,
-        errorElement: <PageNotFound/>
+        element: [<Header/>, <App/>],
+        errorElement: <PageNotFound/>,
     },
     {
-        path: "/register",
-        element: <Register/>, 
-    },
-    {
-        path: "/login",
-        element: <Login/>, 
-    },
-    {
-        path: "/logout",
-        element: <h2>logout</h2>, 
+        path: "/auth/",
+        element: <Header/>,
+        children:[
+            {
+                path: "register",
+                element: <Register/>
+            },
+            {
+                path: "login",
+                element: <Login/>, 
+            },
+            {
+                path: "logout",
+                element: <h2>logout</h2>, 
+            }
+        ]
     }
 ]);
-
+ 
 export default router;

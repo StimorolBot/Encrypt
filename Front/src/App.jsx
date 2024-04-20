aad909b019cd5ed47f5f4079974eefe94a614868
Maxim <maximbotpy@gmail.com>
2024-04-20 21:40:59 +0300

    убрал Header

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,12 +1,8 @@
 import { Encrypt } from "/src/page/Encrypt";
-import { Header } from "/src/components/header/Header";
 
 
 export function App() {
     return (
-        <>  
-            <Header/>
-            <Encrypt/>
-        </>
-    )
+        <Encrypt/>
+    );
 }

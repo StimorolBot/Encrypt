b29a8ef7cf5edec1b4daa2b7020e2066195b4ecc
Maxim <maximbotpy@gmail.com>
2024-04-20 21:50:31 +0300

    добавил функция проверки типа токена

--- Back/src/app/auth/token.py
+++ Back/src/app/auth/token.py
@@ -1,15 +1,17 @@
 import jwt
-from core.config import config
+from core.setting import setting
+from fastapi import status, HTTPException
 from datetime import datetime, timedelta, timezone
+from jwt.exceptions import InvalidSignatureError, ExpiredSignatureError
 
 
 class JWTToken:
     @staticmethod
     async def encoded_jwt(payload: dict,
-                          algorithm: str = config.auth_jwt.algorithm,
-                          private_key: str = config.auth_jwt.private_path_key.read_text(),
+                          algorithm: str = setting.auth_jwt.algorithm,
+                          private_key: str = setting.auth_jwt.private_path_key.read_text(),
                           expire_timedelta: timedelta | None = None,
-                          expire_minutes: int = config.auth_jwt.access_token_expire_minutes):
+                          expire_minutes: int = setting.auth_jwt.access_token_expire_minutes):
         to_encode = payload.copy()
         now = datetime.now(timezone.utc)
         if expire_timedelta:
@@ -21,8 +23,8 @@ class JWTToken:
 
     @staticmethod
     async def decoded_jwt(token: str,
-                          algorithms: str = config.auth_jwt.algorithm,
-                          public_key: str = config.auth_jwt.public_path_key.read_text()):
+                          algorithms: str = setting.auth_jwt.algorithm,
+                          public_key: str = setting.auth_jwt.public_path_key.read_text()):
         return jwt.decode(token, public_key, algorithms=algorithms)
 
     @staticmethod
@@ -30,3 +32,20 @@ class JWTToken:
         jwt_payload = {"type": token_type}
         jwt_payload.update(token_data)
         return await JWTToken.encoded_jwt(payload=jwt_payload, expire_timedelta=expire_timedelta)
+
+    @staticmethod
+    async def validate_token_type(token: str, token_type) -> dict:
+        if not token:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Пользователь не авторизован")
+
+        try:
+            payload = await JWTToken.decoded_jwt(token)
+            if payload.get("type") != token_type:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверный тип токена")
+            return payload
+
+        except InvalidSignatureError:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Невалидный токен")
+
+        except ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Время жизни токена истекло")

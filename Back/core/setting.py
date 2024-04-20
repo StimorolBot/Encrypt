3ba601d9e9f43c68324e88ffa21e0d03d27faedb
Maxim <maximbotpy@gmail.com>
2024-04-20 21:51:11 +0300

    добавил настройки для токена

--- Back/core/setting.py
+++ Back/core/setting.py
@@ -1,20 +1,29 @@
+from pathlib import Path
+from pydantic import BaseModel
 from redis import asyncio as aioredis
+from fastapi.security import HTTPBearer
 from passlib.context import CryptContext
-from fastapi.security import APIKeyCookie
-from fastapi.templating import Jinja2Templates
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+BASE_DIR = Path(__file__).parent.parent
 
-class Config(BaseSettings):
+
+class AuthJWT(BaseModel):
+    private_path_key: Path = BASE_DIR / ".secret" / "jwt-private.pem"
+    public_path_key: Path = BASE_DIR / ".secret" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 30
+    refresh_token_expire_days: int = 30
+
+
+class Setting(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_HOST: str
     DB_PORT: str
     DB_NAME: str
 
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    auth_jwt: AuthJWT = AuthJWT()
 
     BASE_PATH: str
     model_config = SettingsConfigDict(env_file="core/.env")
@@ -24,8 +33,7 @@ class Config(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
-config = Config()
-template = Jinja2Templates(directory="../Front/")
+setting = Setting()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-cookie_scheme = APIKeyCookie(name="access_token")
 redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+http_bearer = HTTPBearer(auto_error=False)

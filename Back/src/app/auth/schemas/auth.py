6baa9eb85b5b224dc747a3772ce685f8736238b0
Maxim <maximbotpy@gmail.com>
2024-04-20 21:42:53 +0300

    изменил базовую схему, добавил схему для входа

--- Back/src/app/auth/schemas/auth.py
+++ Back/src/app/auth/schemas/auth.py
@@ -4,21 +4,26 @@ from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class BaseUser(BaseModel):
-    user_name: str
     email: EmailStr
+
+
+class UserCreate(BaseUser):
+    user_name: str
+    password: str
     is_active: bool = False
     is_superuser: bool = False
     is_verified: bool = False
 
-
-class UserCreate(BaseUser):
-    password: str
-
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserLogin(BaseUser):
+    password: str
 
 
 class UserRead(BaseUser):
     id: UUID
+    user_name: str
     date_register: datetime
 
     model_config = ConfigDict(from_attributes=True)

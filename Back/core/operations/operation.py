7eefd0911594c6c23fbb0a5b3daf4cc0deec315f
Maxim <maximbotpy@gmail.com>
2024-04-20 21:55:58 +0300

    добавил функцию конвертирования дня в секунды

--- Back/core/operations/operation.py
+++ Back/core/operations/operation.py
@@ -1,5 +1,10 @@
 from uuid import uuid4
+from datetime import timedelta
 
 
 def generate_uuid():
     return str(uuid4())
+
+
+def get_seconds(day: int) -> int:
+    return int(timedelta(days=day).total_seconds())

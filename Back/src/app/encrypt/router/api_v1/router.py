b31124dacb571c46b31c0db23a475b9d7cabfcfb
Maxim <maximbotpy@gmail.com>
2024-04-26 21:50:07 +0300

    переделал роутеры

--- Back/src/app/encrypt/router/api_v1/router.py
+++ Back/src/app/encrypt/router/api_v1/router.py
@@ -1,33 +1,44 @@
 from typing import TYPE_CHECKING
 from fastapi.responses import FileResponse
-from fastapi import APIRouter, UploadFile, File, Form, status, Depends, Request
+from fastapi import APIRouter, UploadFile, File, Form, status, Depends
 
 from core.db import get_async_session
 
+from core.setting import http_bearer
 from src.app.encrypt.operations import encrypt
 from src.app.auth.user_manager import UserManager
-from src.app.encrypt.file_manager import FileManager
+from src.app.encrypt.models.model import FileTable
+from src.app.encrypt.file_manager import file_manager
 from src.app.encrypt.schemas.schemas import EncryptSchemas
 
 if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
     from src.app.auth.schemas.auth import UserRead
+    from sqlalchemy.ext.asyncio import AsyncSession
 
-router = APIRouter(tags=["encrypt"])
+router = APIRouter(tags=["encrypt"], dependencies=[Depends(http_bearer)])
 
 
 @router.get("/")
-async def get_home_page(re: Request, token: dict = Depends(UserManager.get_jwt_token), session: "AsyncSession" = Depends(get_async_session), ):
-    print(re.cookies["access_token"]) #
-    return await FileManager.get_info(session=session, email=token["email"])
+async def get_home_page(current_user: "UserRead" = Depends(UserManager.get_current_user),
+                        session: "AsyncSession" = Depends(get_async_session)):
+    file_info = await file_manager.get_exists_files(session=session, field=FileTable.email, value=current_user.email)
+    return file_info
 
 
 @router.post("/file", status_code=status.HTTP_200_OK)
-async def file_operations(password: str = Form(...), upload_file: UploadFile = File(...),
-                          current_user: "UserRead" = Depends(UserManager.get_current_user),
-                          session: "AsyncSession" = Depends(get_async_session)) -> FileResponse:
-    EncryptSchemas(file_name=upload_file.filename, file_size=upload_file.size, password=password)
-    await FileManager().check_exists_file(session=session, file_name=upload_file.filename, dir_=current_user.email)
-    await FileManager().save_file(dir_=current_user.email, file_name=upload_file.filename, file=upload_file, session=session)
-    output_path, output_file = await encrypt(dir_=current_user.email, file_name=upload_file.filename, password=password)
-    return FileResponse(path=output_path, filename=output_file, media_type='multipart/form-data')
+async def upload_file(password: str = Form(...), file: UploadFile = File(...),
+                      current_user: "UserRead" = Depends(UserManager.get_current_user),
+                      session: "AsyncSession" = Depends(get_async_session)) -> FileResponse:  # -> dict:  #
+    EncryptSchemas(file_name=file.filename, file_size=file.size, password=password)  # проверка пароля на запрещенные символы
+    await file_manager.add_file(session=session, file_name=file.filename,
+                                email=current_user.email, file=file, field=FileTable.name)
+    output_path, output_file = await encrypt(dir_=current_user.email, file_name=file.filename, password=password, session=session)
+
+    return FileResponse(path=output_path, filename=output_file, media_type='multipart/form-data',
+                        headers={"Content-Disposition": output_file})
+
+
+@router.get("/file/{download}/{file_name}")
+async def download_file(download: str, file_name: str):
+    # проверка пользователя и папки
+    ...

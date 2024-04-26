cdda62de64caef08f0fd080bd5cf5fd19d2d9cd5
Maxim <maximbotpy@gmail.com>
2024-04-26 19:43:42 +0300

    добавил валидацию для полей

--- Back/src/app/auth/schemas/auth.py
+++ Back/src/app/auth/schemas/auth.py
@@ -1,15 +1,16 @@
 from uuid import UUID
 from datetime import datetime
-from pydantic import BaseModel, EmailStr, ConfigDict
+from core.validator.validator import BaseValidator
+from pydantic import EmailStr, ConfigDict, Field
 
 
-class BaseUser(BaseModel):
-    email: EmailStr
+class BaseUser(BaseValidator):
+    email: EmailStr = Field(..., min_length=8, max_length=20)
 
 
 class UserCreate(BaseUser):
-    user_name: str
-    password: str
+    user_name: str = Field(..., min_length=4, max_length=20)
+    password: str = Field(..., min_length=4, max_length=24)
     is_active: bool = False
     is_superuser: bool = False
     is_verified: bool = False
@@ -18,12 +19,12 @@ class UserCreate(BaseUser):
 
 
 class UserLogin(BaseUser):
-    password: str
+    password: str = Field(..., min_length=4, max_length=24)
 
 
 class UserRead(BaseUser):
     id: UUID
-    user_name: str
+    user_name: str = Field(..., min_length=4, max_length=20)
     date_register: datetime
 
     model_config = ConfigDict(from_attributes=True)

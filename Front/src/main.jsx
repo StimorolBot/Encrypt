91c31a962f33b3efe3d1944ba70303078ce34547
Maxim <maximbotpy@gmail.com>
2024-04-29 23:40:21 +0300

    обернул app в BrowserRouter

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { App } from './App';
 import ReactDOM from 'react-dom/client';
-import router from '/src/router/router';
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router}/>
-
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>   
 )

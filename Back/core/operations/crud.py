41ae6bfab8d16b60de79c3ad056b7c3e2f4fba03
Maxim <maximbotpy@gmail.com>
2024-04-29 23:35:24 +0300

    добавил функцию удаления записей

--- Back/core/operations/crud.py
+++ Back/core/operations/crud.py
@@ -1,5 +1,5 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import select, func, update
+from sqlalchemy import select, func, update, delete
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -16,14 +16,26 @@ class Crud:
         return stmt
 
     @staticmethod
-    async def read_one(session: "AsyncSession", table: "DeclarativeAttributeIntercept",
-                       field: "DeclarativeAttributeIntercept", value: str):
+    async def read(session: "AsyncSession", table: "DeclarativeAttributeIntercept",
+                   field: "DeclarativeAttributeIntercept", value: str, mode: str = "one"):
         query = select(table).where(func.lower(field) == func.lower(value))
         results = await session.execute(query)
-        return results.unique().scalar_one_or_none()
+
+        if mode == "one":
+            return results.unique().scalar_one_or_none()
+
+        elif mode == "all":
+            return [item for items in results.all() for item in items]
 
     @staticmethod
     async def update(session: "AsyncSession", table: "DeclarativeAttributeIntercept", field, field_val, data: dict):
-        query = update(table).where(field == field_val).values(**data)
+        query = update(table).where(func.lower(field) == func.lower(field_val)).values(**data)
         await session.execute(query)
         await session.commit()
+
+    @staticmethod
+    async def delete(session: "AsyncSession", table: "DeclarativeAttributeIntercept", field, field_val):
+        stmt = delete(table).where(func.lower(field) == func.lower(field_val))
+        await session.execute(stmt)
+        await session.commit()
+        return stmt

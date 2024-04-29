5440fe780033308472cbb54a394244617c67fa34
Maxim <maximbotpy@gmail.com>
2024-04-29 23:38:23 +0300

    перенес роутеры в отдельный файл

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,27 +1,8 @@
-import { useState } from "react";
-import { AuthContex } from "./context/main";
-import { Encrypt } from "/src/page/Encrypt";
-import { Login } from '/src/page/auth/Login';
-import { Route, Routes, Navigate } from "react-router-dom";
-import { Register } from '/src/page/auth/Register';
-import { Header } from "/src/components/header/Header";
-import { PageNotFound } from "/src/page/error/PageNotFound";
+import { MyRouter } from "./router/router";
 
 
 export function App() {
-    const [isAuth, setIsAuth] = useState(true);
-
     return (
-        <AuthContex.Provider value={{isAuth, setIsAuth}}>
-            <Routes>
-                <Route path="/" element={<Header/>}>
-                    <Route index element={<Encrypt/>} />
-                    <Route path="auth/logout" element={<h1>выход</h1>} />
-                    <Route path="auth/login" element={<Login/>} />
-                    <Route path="auth/register" element={<Register/>} />
-                    <Route path="*" element={<PageNotFound/>} />
-                </Route>
-            </Routes>
-        </AuthContex.Provider>
+        <MyRouter/>
     );
 }

bb16cc6d39f941ed1ba05553d7b709ace588b665
Maxim <maximbotpy@gmail.com>
2024-05-06 22:25:45 +0300

    устанавливаю токен в куки

--- Front/src/page/auth/Login.jsx
+++ Front/src/page/auth/Login.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";   
 import api from "/src/api/api";
-import { redirect, Navigate } from "react-router-dom";
+import { useState } from "react";  
+import cookies from "/src/api/cookies"
+import { Link } from "react-router-dom";
 import { MainBtn } from "/src/components/ui/btn/MainBtn";
 import { MainForm } from "/src/components/ui/form/MainForm";
 import { MainInput } from "/src/components/ui/input/MainInput";
 
 import "/src/style/components/page/auth/login.sass";
-
 
 export function Login() {
     const [userData, setUserData] = useState({"email": "", "password":""});
@@ -17,14 +17,23 @@ export function Login() {
         await api.post("/auth/login", userData)
             .then((response) => {
                 localStorage.setItem("access_token", response.data["access_token"]);
+                cookies.set("refresh_token", response.data["refresh_token"],
+                    { maxAge: response.data["refresh_max_age"] }
+                );
             })
-            .catch((error) =>{
-                console.log(error);
+            .catch((error) => {
+                if (error.response["status"] === 400){
+                    alert(error.response.data["detail"]);
+                }
             });
         };
 
+    // использовать useEffect
+    //if (isAuth == true)
+      //  return <Navigate to={"/"}/>    
+
     return (
-        <div className="wrapper wrapper__body">
+        <div className="wrapper login__wrapper">
             <MainForm onSubmit={postlogin}>
                 <MainInput lblText={"Логин"} maxLength={24}
                     type="text" placeholder=" " required
@@ -33,7 +42,13 @@ export function Login() {
                 <MainInput lblText={"Пароль"} maxLength={24}
                     type="password" placeholder=" " required
                     onChange={(event) => setUserData({...userData, password: event.target.value})}
-                /> 
+                />
+                <div className="login__reset-password-container">
+                    <Link className="login__reset-password" to="/auth/reset-password">
+                        Сбросить пароль
+                    </Link>
+                </div> 
+
                 <MainBtn>Войти</MainBtn>
             </MainForm>
         </div>

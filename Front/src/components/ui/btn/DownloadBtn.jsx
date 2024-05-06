69b3f365d2dd899474cb5ebfa74fe40e2eb6db33
Maxim <maximbotpy@gmail.com>
2024-05-06 22:23:13 +0300

    внес мелкие изменения

--- Front/src/components/ui/btn/DownloadBtn.jsx
+++ Front/src/components/ui/btn/DownloadBtn.jsx
@@ -1,8 +1,7 @@
 import { MainBtn } from "./MainBtn";
 
-import "/src/style/components/ui/btn/download_btn.sass";
 
-export function DownloadBtn({btnText,...props}){
+export function DownloadBtn({btnText, ...props}){
     return(
         <a className="download-btn" {...props}>
             <MainBtn type="button">

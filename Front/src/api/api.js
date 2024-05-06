0b19fbc2576f8ac6ce0dff30b4164fe28c35a015
Maxim <maximbotpy@gmail.com>
2024-05-06 22:22:05 +0300

    добавил перехватчики для ошибок

--- Front/src/api/api.js
+++ Front/src/api/api.js
@@ -5,7 +5,7 @@ export const api = axios.create({baseURL:"http://127.0.0.1:8000",
     sameSite: "none",
 });
 
-api.interceptors.request.use((config)=> {
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("access_token");
     
     if (!token){
@@ -16,5 +16,12 @@ api.interceptors.request.use((config)=> {
 
     return config;
 });
+
+api.interceptors.response.use((response) => {
+    return response;
+    }, (error) => {
+        return error.response;
+    }
+);
 
 export default api;

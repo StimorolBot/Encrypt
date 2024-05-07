34017cbe3431cb8ed9c78ecb58c1fd43931f4346
Maxim <maximbotpy@gmail.com>
2024-05-07 22:01:00 +0300

    добавил шаблон для подтверждения почты

---
+++ Back/core/bg_tasks/smtp_template.py
@@ -0,0 +1,42 @@
+from typing import TYPE_CHECKING
+from core.operations.operation import generate_code
+
+if TYPE_CHECKING:
+    from pydantic import EmailStr
+    from email.message import EmailMessage
+
+
+def email_confirm(email: "EmailMessage", user_name: str, location: str, user_email: "EmailStr") -> list:
+    code_confirm = generate_code()
+    email["Subject"] = "Подтверждение почты"
+
+    email.set_content(f"""
+        <div style="color: #e3e7e9;">
+            <div class="email__container" style="margin-top: 20px; font-size: 20px; border-radius: 10px;
+                max-width: 800px; background-color: hsl(223, 10%, 25%); max-height: 600px;">
+                
+                <div class="wrapper" style="padding: 30px; display: block; overflow: hidden;">
+                    <h2 class="email-username" style="text-align: center; overflow: hidden"> 
+                        Здравствуйте, {user_name}
+                    </h2>
+                    
+                    <div class="email__code-container" style="overflow: hidden;">
+                        <p class="email-code-msg"> Код, необходимый для регистрации {user_email}</p>
+                        <p class="email-code" style="color: #1e96ff; font-weight: 800; font-size: 60px; text-align: center;
+                            background-color: #2a2a2d; padding: 15px; border-radius: 5px;"> {code_confirm} </p>
+                    </div>
+                    
+                    <div class="email__info-container" style="overflow: hidden;">
+                        <p class="email-info">
+                            Вы получили это письмо из-за попытки регистрации учетной записи
+                            из браузера или с мобильного устройства по адресу {location}
+                        </p>
+                        <p class="email-info" style="padding-top: 20px; overflow: hidden;">
+                            Если вы не пытались создать учетную запись, проигнорируйте это письмо
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div> """, subtype="html")
+
+    return [email, code_confirm]

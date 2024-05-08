f268e3e02118e8f6011c29cfee05bd5e72138acd
Maxim <maximbotpy@gmail.com>
2024-05-08 16:29:21 +0300

    прокинул пропсы для fileInfo

--- Front/src/components/sidebar/Sidebar.jsx
+++ Front/src/components/sidebar/Sidebar.jsx
@@ -3,14 +3,14 @@ import {FileInfo} from "/src/components/sidebar/FileInfo";
 import "/src/style/components/sidebar/sidebar.sass";
 
 
-export function Sidebar() {
+export function Sidebar({ fileData, setFileData}) {
     return (
         <>  
             <input className="burger-checkbox" type="checkbox" id="burger-checkbox"/>
             <label className="burger" htmlFor="burger-checkbox"></label>
             <div className="sidebar">
                 <div className="wrapper sidebar__wrapper">
-                    <FileInfo/>
+                    <FileInfo fileData={ fileData } setFileData={ setFileData }/>
                 </div>
             </div>
         </>

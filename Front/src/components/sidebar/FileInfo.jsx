8cedd7caaa5dff0d2c5253c7e212997bdd68f66d
Maxim <maximbotpy@gmail.com>
2024-05-08 16:23:11 +0300

    перенес запрос файлов в Encrypt

--- Front/src/components/sidebar/FileInfo.jsx
+++ Front/src/components/sidebar/FileInfo.jsx
@@ -1,7 +1,4 @@
 import { api } from "/src/api/api"
-import { useEffect, useState } from "react";
-import { useFetch } from "../hook/useFetch";
-import { getFileInfo } from "../../api/http";
 import { createUrlDownload } from "../../api/url";
 import { ContextMenu } from "../ui/menu/ContextMenu";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
@@ -9,32 +6,19 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "/src/style/components/sidebar/file_info.sass"
 
 
-export function FileInfo() {
-    const [data, setData] = useState({
-        "user_name": "", "email": "", "file_name": [] 
-    });
-    
-    const [fetchFile, isLoad, errorResponse] = useFetch(async () => {
-            const response = await getFileInfo();
-            setData({...data,
-                user_name: response["user_name"],
-                email: response["email"],
-                file_name: response["path"]["file_name"].map(file => file["name"])
-            });    
-        }
-    );
-  
+export function FileInfo({ fileData, setFileData }) {
+
     const deleteFile = async (event, index) =>  {
         event.preventDefault();
-        const name = data["file_name"][index];
+        const name = fileData["file_name"][index];
         // Удаление элемента из списка
-        setData({...data, file_name: data["file_name"].filter( item => item != name )}); 
+        setFileData({...fileData, file_name: fileData["file_name"].filter( item => item != name )}); 
         await api.delete("/file/file-delete", {data: {"name": name}})
     }
 
     const downloadFile = async (event, index) => {
         event.preventDefault();
-        const name = data["file_name"][index];
+        const name = fileData["file_name"][index];
         
         await api.get(`/file/download/user@mail.ru/${name}`, { responseType: 'blob' })
         .then((response) => {
@@ -45,24 +29,22 @@ export function FileInfo() {
         });
     }
 
-    useEffect(() => {
-        fetchFile();
-    }, []);
-
     return (
-        <> 
-        {/* <div className="sidebar__username-container" onClick={() => t()}>
-            <h3 className="sidebar-username">{'data[0]["user_name"]'}</h3>
-        </div> */}
-        
-        <ul className="sidebar__file-container" >
+        <ul className="sidebar__file-container">
+
+            <div className="sidebar__username-container">
+                <h3 className="sidebar-username">
+                    {fileData["user_name"] !== undefined && fileData["user_name"]}
+                </h3>
+            </div>
+
             <div className="wrapper file__wrapper">
-                { data === undefined ? <h4 className="file-none">У вас еще нет файлов</h4>
-                : data["user_name"] !== undefined &&
-                    <TransitionGroup className="todo-list">
-                        { data["file_name"].map((item, index) =>
-                            <CSSTransition classNames="sidebar__file-ransition" 
-                                key={index}  timeout={ 100 }>
+                { fileData === undefined ? <h4 className="file-none">У вас еще нет файлов</h4>
+                : fileData["user_name"] !== undefined &&
+                    <TransitionGroup className="file-list">
+                        { fileData["file_name"].map((item, index) =>
+                            <CSSTransition classNames="sidebar__file-transition" 
+                                key={index}  timeout={ 500 }>
                                 
                                 <li className="sidebar__file-info">
                                     <img className="sidebar-file-ico" src="../../public/file-regular.svg" alt="file.ico"/>
@@ -87,6 +69,5 @@ export function FileInfo() {
                 }
             </div>
         </ul>
-        </>
     );
 }

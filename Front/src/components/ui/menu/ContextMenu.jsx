915b1c13de6988fe678650e316ee319bf38d69ce
Maxim <maximbotpy@gmail.com>
2024-05-08 16:21:48 +0300

    перенес useEffect в hook

--- Front/src/components/ui/menu/ContextMenu.jsx
+++ Front/src/components/ui/menu/ContextMenu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useClickOutside } from "../../hook/useClickOutside";
 
 import "/src/style/components/ui/menu/context_menu.sass"
@@ -6,30 +6,19 @@ import "/src/style/components/ui/menu/context_menu.sass"
 
 export function ContextMenu({ children }) {
     const [visible, setVisible] = useState(false);
-    let menuRef = useRef();
+    const menuRef = useRef();
 
-    useEffect(() => {
-        let handler = (e) => {
-            if (!menuRef.current.contains(e.target)){
-                setVisible(false);
-            }
-        };
-        document.addEventListener("mousedown", handler);
-        return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-    });
-
+    useClickOutside(menuRef, setVisible);
+    
     return(
-        <menu className="context-menu" ref={menuRef}>
+        <menu className="context-menu" ref={ menuRef }>
             <ul className="context-menu-btn" onClick={() => setVisible((state) => !state)}>
                 <li className="context-menu-btn-point"></li>
                 <li className="context-menu-btn-point"></li>
                 <li className="context-menu-btn-point"></li>
             </ul>
 
-            <ul className={`context-menu__container_${visible}`}
-                onClick={(e) => useClickOutside()}>
+            <ul className={`context-menu__container_${visible}`}>
                 { children }
             </ul>   
         </menu>

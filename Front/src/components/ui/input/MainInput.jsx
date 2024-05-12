510eba72898eff702b46ead422d382cf680ee9f9
Maxim <maximbotpy@gmail.com>
2024-05-12 20:28:22 +0300

    добавил label для вывода ошибки

--- Front/src/components/ui/input/MainInput.jsx
+++ Front/src/components/ui/input/MainInput.jsx
@@ -1,13 +1,17 @@
 import "/src/style/components/ui/input/main_input.sass"
 
-export function MainInput({lblText, children, ...props}) {
+export function MainInput({ lblText, register, errorsMessage, children, ...props }) {
+
     return(
         <div className="input__container">
-            <input className="input-main" {...props}/>
+            <input className="input-main"  {...register} {...props}/>
             <label className="input-lbl">
-                <p className="input-lbl-text">{lblText}</p>
+                <p className="input-lbl-text">{ lblText }</p>
             </label>
             { children }
+            <label className="label-error">
+                { errorsMessage }
+            </label> 
         </div>
     );
 };

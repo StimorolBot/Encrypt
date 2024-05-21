9fe4384799098e0fc9fba526ec28596ad58cc8d1
Maxim <maximbotpy@gmail.com>
2024-05-21 14:52:25 +0300

    добавил условие для отправки типа письма

--- Back/core/bg_tasks/tasks.py
+++ Back/core/bg_tasks/tasks.py
@@ -1,21 +1,33 @@
 import ssl
 import smtplib
+from pydantic import EmailStr
 from email.message import EmailMessage
-from core.bg_tasks.smtp_template import email_confirm
+from fastapi import status, HTTPException
+
+from core.logger import user_logger
 from core.bg_tasks.setting import smtp_setting, celery
+from core.bg_tasks.smtp_template import email_confirm, reset_password
 
 
-@celery.task(name="send_email")
-def send_email(ip_address: str, user_name: str, user_email=smtp_setting.ADMIN_EMAIL) -> str:
+@celery.task(name="send_email", max_retries=3, default_retry_delay=30)
+def send_email(ip_address: str, type_email: str, user_agent: str, user_email: EmailStr = smtp_setting.ADMIN_EMAIL) -> str:
     email = EmailMessage()
     email["From"] = user_email
     email["To"] = smtp_setting.ADMIN_EMAIL
     context = ssl.create_default_context()
 
-    msg, code = email_confirm(email=email, user_name=user_name, location=ip_address, user_email=user_email)
+    match type_email:
+        case "email_confirm":
+            email_template, code_confirm = email_confirm(email=email, location=ip_address, user_email=user_email, user_agent=user_agent)
+            user_logger.info(f"Запрос на создание аккаунта {user_email}")
+        case "reset_password":
+            email_template, code_confirm = reset_password(email=email, location=ip_address, user_email=user_email, user_agent=user_agent)
+            user_logger.info(f"Запрос на сброс пароля {user_email}")
+        case _:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Невалидный тип письма")
 
     with smtplib.SMTP_SSL(smtp_setting.host, smtp_setting.port, context=context) as server:
         server.login(smtp_setting.ADMIN_EMAIL, smtp_setting.PASSWORD)
-        server.send_message(msg)
+        server.send_message(email_template)
 
-    return code
+    return code_confirm

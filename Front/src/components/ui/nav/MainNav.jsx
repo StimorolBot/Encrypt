d68c75eb050417af88fd19360a209344b65d6400
Maxim <maximbotpy@gmail.com>
2024-05-21 11:17:57 +0300

    заменил if на тернарный оператор

--- Front/src/components/ui/nav/MainNav.jsx
+++ Front/src/components/ui/nav/MainNav.jsx
@@ -1,18 +1,14 @@
+import "./main_nav.sass";
 import { Link } from "react-router-dom";
-import "/src/style/components/ui/nav/main_nav.sass";
 
 
-export function MainNav({navItems, active}) {
-    let classNav = ["header__auth-items"];
-
-    if (active)
-        classNav.push("header__auth-items_active");
-    else
-        classNav = ["header__auth-items"];
-        
+export function MainNav({ navItems, active }) {
     return(
-        <nav className={classNav.join(" ")}>
-            {navItems.map((item) => 
+        <nav className={ active ? 
+            "header__auth-items header__auth-items_active" 
+            : "header__auth-items"}
+        >
+            { navItems.map((item) => 
                 <Link className="header__auth-item" to={item.url} key={item.url}>
                     {item.name}
                 </Link>

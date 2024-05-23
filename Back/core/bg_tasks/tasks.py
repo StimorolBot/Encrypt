38ca4b25ff0f3d579b008850fdefe6747f8f0449
Maxim <maximbotpy@gmail.com>
2024-05-23 23:08:34 +0300

    добавил разблокировку пользователя, вынес генерацию письма в отдельную функцию

--- Back/core/bg_tasks/tasks.py
+++ Back/core/bg_tasks/tasks.py
@@ -1,33 +1,33 @@
 import ssl
 import smtplib
+import asyncio
 from pydantic import EmailStr
 from email.message import EmailMessage
-from fastapi import status, HTTPException
 
-from core.logger import user_logger
+
 from core.bg_tasks.setting import smtp_setting, celery
-from core.bg_tasks.smtp_template import email_confirm, reset_password
+from core.bg_tasks.operations import generate_email, unlock_user
 
 
-@celery.task(name="send_email", max_retries=3, default_retry_delay=30)
+@celery.task(name="send_email", max_retries=3, default_retry_delay=3, limit=3)
 def send_email(ip_address: str, type_email: str, user_agent: str, user_email: EmailStr = smtp_setting.ADMIN_EMAIL) -> str:
     email = EmailMessage()
     email["From"] = user_email
     email["To"] = smtp_setting.ADMIN_EMAIL
     context = ssl.create_default_context()
 
-    match type_email:
-        case "email_confirm":
-            email_template, code_confirm = email_confirm(email=email, location=ip_address, user_email=user_email, user_agent=user_agent)
-            user_logger.info(f"Запрос на создание аккаунта {user_email}")
-        case "reset_password":
-            email_template, code_confirm = reset_password(email=email, location=ip_address, user_email=user_email, user_agent=user_agent)
-            user_logger.info(f"Запрос на сброс пароля {user_email}")
-        case _:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Невалидный тип письма")
+    code_confirm, email_template = generate_email(
+        ip_address=ip_address, type_email=type_email,
+        user_agent=user_agent, user_email=user_email, email=email
+    )
 
     with smtplib.SMTP_SSL(smtp_setting.host, smtp_setting.port, context=context) as server:
         server.login(smtp_setting.ADMIN_EMAIL, smtp_setting.PASSWORD)
         server.send_message(email_template)
 
     return code_confirm
+
+
+@celery.task(name="unlock_user", max_retries=3, default_retry_delay=10)
+def unlock_user_task(email):
+    asyncio.run(unlock_user(email))
